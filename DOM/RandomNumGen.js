bffa0aca05a77ce73a76e801b73931ac73014d2f
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-09-08 22:17:19 +0530

    8th sept'25

---
+++ DOM/RandomNumGen.js
@@ -0,0 +1,10 @@
+var rand=Math.random()// give random Number from 0.0 to 0.9
+console.log(rand)
+console.log(Math.floor(rand))
+console.log(rand*10)
+console.log(Math.floor(rand*10))
+
+
+// in order to get it from 1 to 10 then we should "ADD +1"
+console.log(rand+1)
+                                                                                                                                                                            
